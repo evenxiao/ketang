42ad3615f120c662a1d033eac34947611a2eab48
evenxiao <543616070@qq.com>
2016-12-07 17:05:44 +0800

    修改

--- app/Lib/Action/Admin/NewsAction.class.php
+++ app/Lib/Action/Admin/NewsAction.class.php
@@ -97,7 +97,7 @@ class NewsAction extends BaseAction {
     }
 
     /**
-     * 修改视频
+     * 修改资讯
      */
     public function edit(){
         $id = intval(I('param.id',0));
@@ -105,11 +105,13 @@ class NewsAction extends BaseAction {
             $data = I('post.');
             $tags = I('post.taglist');
             $attachs = I('post.attach');
+            $attach_names = I('post.attach_name');
             if(!empty($data) || !$id){
                 unset($data['beizhu']);
                 unset($data['editorValue']);
                 unset($data['taglist']);
                 unset($data['attach']);
+                unset($data['attach_name']);
 
                 $data['create_time'] = date('Y-m-d H:i:s');
                 $status = $this->contentModel->editContent($id, $data);
@@ -123,11 +125,22 @@ class NewsAction extends BaseAction {
                        $tagData[] = $tmp;
                    }
                    //删除原来的
-                   D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->save(array('status'=>2));
+                   D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->save(array('status'=>2, 'update_time'=>date('Y-m-d H:i:s')));
                    //插入新的
                    D('TagContent')->addAll($tagData);
                }
-
+               if($attachs){
+                        foreach($attachs as $key=>$val){
+                            
+                            $tmp['title'] = $attach_names[$key];
+                            $tmp['attach_url'] = $val;
+                            $tmp['content_id'] = $id;
+                            $tmp['create_time'] = date('Y-m-d H:i:s');
+                            $attachData[] = $tmp;
+                        }
+                         D('Attach')->where(array('content_id'=>$id, 'status'=>1))->save(array('status'=>2, 'update_time'=>date('Y-m-d H:i:s')));
+                        D('Attach')->addAll($attachData);
+                }   
                 $result['status'] = $status ? 1 : 0;
                 $result['message'] = $result['status'] ? '修改成功' : '修改失败';
                 $this->ajaxReturn($result);
@@ -141,13 +154,14 @@ class NewsAction extends BaseAction {
 
         //echo $id;
         $where['id'] = $id;
-        $data['info'] = $this->contentModel->getVideoInfo($where);
-        $data['cates'] = D('Cate')->getCateList(array('type'=>1, 'status'=>1));
+        $data['info'] = $this->contentModel->getNewsInfo($where);
+        $data['cates'] = D('Cate')->getCateList(array('type'=>2, 'status'=>1));
         $data['tags'] = D('Tag')->where(array('status'=>1))->select();
 
         // 该内容已有的标签
 
-        D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->getField();
+        $data['tag_ids'] = D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->getField('tag_id', true);
+
         $this->assign('data', $data);
         $this->display();
     }
@@ -175,6 +189,39 @@ class NewsAction extends BaseAction {
             $this->ajaxReturn($data);
         }
 
+    }
+
+     /**
+     *  评论列表
+     *
+     */
+    public function commentList(){
+        $id = I('param.id', 0);
+
+        $data['commentList'] = D('Comment')->where(array('content_id'=>$id, 'status'=>array('in', array(0,1))))->select();
+
+        $this->assign('data', $data);
+
+        $this->display();
+    }
+
+    /**
+    *
+    * 审核评论
+    */
+    public function checkComment(){
+        if(IS_AJAX){
+             $id = I('param.id', 0);
+             $info = D('Comment')->where(array('id'=>$id))->find(); 
+             if($info['status'] != 0){
+                $this->ajaxReturn(array('status'=>0, 'message'=>'该评论已审核,无须重新审核！'));
+             }
+             $status = D('Comment')->where(array('id'=>$id, 'status'=>0))->save(array('status'=>$status,'update_time'=>date('Y-m-d H:i:s')));  
+             $result['status'] = $status ? 1 : 0;
+             $result['message'] = $result['status'] == 1 ? '审核成功' : '审核失败';
+             $this->ajaxReturn($result);
+        } 
+        
     }
 
 }
