59030a20b58073317658637b710dce5df7af4c32
evenxiao <543616070@qq.com>
2016-12-07 17:11:52 +0800

    ...

--- App/Lib/Action/Admin/VideoAction.class.php
+++ App/Lib/Action/Admin/VideoAction.class.php
@@ -105,11 +105,13 @@ class VideoAction extends BaseAction {
             $data = I('post.');
             $tags = I('post.taglist');
             $attachs = I('post.attach');
+            $attach_names = I('post.attach_name');
             if(!empty($data) || !$id){
                 unset($data['beizhu']);
                 unset($data['editorValue']);
                 unset($data['taglist']);
                 unset($data['attach']);
+                unset($data['attach_name']);
 
                 $data['create_time'] = date('Y-m-d H:i:s');
                 $status = $this->contentModel->editContent($id, $data);
@@ -123,10 +125,23 @@ class VideoAction extends BaseAction {
                        $tagData[] = $tmp;
                    }
                    //删除原来的
-                   D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->save(array('status'=>2));
+                   D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->save(array('status'=>2, 'update_time'=>date('Y-m-d H:i:s')));
                    //插入新的
                    D('TagContent')->addAll($tagData);
                }
+
+                if($attachs){
+                        foreach($attachs as $key=>$val){
+                            
+                            $tmp['title'] = $attach_names[$key];
+                            $tmp['attach_url'] = $val;
+                            $tmp['content_id'] = $id;
+                            $tmp['create_time'] = date('Y-m-d H:i:s');
+                            $attachData[] = $tmp;
+                        }
+                         D('Attach')->where(array('content_id'=>$id, 'status'=>1))->save(array('status'=>2, 'update_time'=>date('Y-m-d H:i:s')));
+                        D('Attach')->addAll($attachData);
+                }
 
                 $result['status'] = $status ? 1 : 0;
                 $result['message'] = $result['status'] ? '修改成功' : '修改失败';
@@ -147,7 +162,8 @@ class VideoAction extends BaseAction {
 
         // 该内容已有的标签
 
-        D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->getField();
+         $data['tag_ids'] = D('TagContent')->where(array('status'=>1, 'content_id'=>$id))->getField('tag_id', true);
+
         $this->assign('data', $data);
         $this->display();
     }
@@ -177,4 +193,36 @@ class VideoAction extends BaseAction {
 
     }
 
+    /**
+     *  评论列表
+     *
+     */
+    public function commentList(){
+        $id = I('param.id', 0);
+
+        $data['commentList'] = D('Comment')->where(array('content_id'=>$id, 'status'=>array('in', array(0,1))))->select();
+
+        $this->assign('data', $data);
+
+        $this->display();
+    }
+
+    /**
+    *
+    * 审核评论
+    */
+    public function checkComment(){
+        if(IS_AJAX){
+             $id = I('param.id', 0);
+             $info = D('Comment')->where(array('id'=>$id))->find(); 
+             if($info['status'] != 0){
+                $this->ajaxReturn(array('status'=>0, 'message'=>'该评论已审核,无须重新审核！'));
+             }
+             $status = D('Comment')->where(array('id'=>$id, 'status'=>0))->save(array('status'=>$status,'update_time'=>date('Y-m-d H:i:s')));  
+             $result['status'] = $status ? 1 : 0;
+             $result['message'] = $result['status'] == 1 ? '审核成功' : '审核失败';
+             $this->ajaxReturn($result);
+        } 
+        
+    }
 }
